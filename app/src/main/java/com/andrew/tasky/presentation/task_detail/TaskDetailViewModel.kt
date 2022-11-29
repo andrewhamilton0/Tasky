97be7d972adcd0988ace51b6c9ebde8f3b0941f9
andrewhamilton0 <acjts425@gmail.com>
2022-11-29 18:35:38 -0500

    -Adds code to TaskDetailFragment.kt and TaskDetailViewModel.kt -Updates register screen and login screen xml code

--- app/src/main/java/com/andrew/tasky/presentation/task_detail/TaskDetailViewModel.kt
+++ app/src/main/java/com/andrew/tasky/presentation/task_detail/TaskDetailViewModel.kt
@@ -1,7 +1,114 @@
 package com.andrew.tasky.presentation.task_detail
 
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.andrew.tasky.domain.models.AgendaItem
+import com.andrew.tasky.domain.repository.AgendaItemRepository
+import com.andrew.tasky.util.AgendaItemType
+import com.andrew.tasky.util.ReminderTime
+import dagger.hilt.android.lifecycle.HiltViewModel
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.LocalTime
+import javax.inject.Inject
+import kotlinx.coroutines.NonCancellable
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
-class TaskDetailViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+@HiltViewModel
+class TaskDetailViewModel @Inject constructor(
+    private val savedStateHandle: SavedStateHandle,
+    private val repository: AgendaItemRepository
+) : ViewModel() {
+
+    private val agendaItemType = AgendaItemType.TASK
+
+    private val _isInEditMode = MutableStateFlow(false)
+    val isInEditMode = _isInEditMode.asStateFlow()
+    fun setEditMode(isEditing: Boolean) {
+        _isInEditMode.value = isEditing
+    }
+
+    private val _isDone = MutableStateFlow(false)
+    val isDone = _isDone.asStateFlow()
+    fun setIsDone(isDone: Boolean) {
+        _isDone.value = isDone
+    }
+
+    private val _title = MutableStateFlow("Blank Title")
+    val title = _title.asStateFlow()
+    fun setTitle(title: String) {
+        _title.value = title
+    }
+
+    private val _description = MutableStateFlow("Blank Description")
+    val description = _description.asStateFlow()
+    fun setDescription(description: String) {
+        _description.value = description
+    }
+
+    private val _selectedStartDate = MutableStateFlow(LocalDate.now())
+    val selectedStartDate = _selectedStartDate.asStateFlow()
+    fun setStartDate(selectedStartDate: LocalDate) {
+        _selectedStartDate.value = selectedStartDate
+    }
+
+    private val _selectedStartTime = MutableStateFlow(LocalTime.now())
+    val selectedStartTime = _selectedStartTime.asStateFlow()
+    fun setStartTime(selectedStartTime: LocalTime) {
+        _selectedStartTime.value = selectedStartTime
+    }
+
+    private val _selectedReminderTime = MutableStateFlow(ReminderTime.TEN_MINUTES_BEFORE)
+    val selectedReminderTime = _selectedReminderTime.asStateFlow()
+    fun setSelectedReminderTime(selectedReminderTime: ReminderTime) {
+        _selectedReminderTime.value = selectedReminderTime
+    }
+
+    fun saveAgendaItem() {
+        val agendaItem = AgendaItem(
+            savedStateHandle.get<AgendaItem>("agendaItem")?.id,
+            type = agendaItemType,
+            isDone = isDone.value,
+            title = title.value,
+            description = description.value,
+            startDateAndTime = LocalDateTime.of(
+                selectedStartDate.value,
+                selectedStartTime.value
+            ),
+            reminderTime = selectedReminderTime.value
+        )
+        viewModelScope.launch {
+            withContext(NonCancellable) {
+                repository.upsert(agendaItem)
+            }
+        }
+    }
+
+    fun deleteAgendaItem() {
+        viewModelScope.launch {
+            withContext(NonCancellable) {
+                savedStateHandle.get<AgendaItem>("agendaItem")?.let {
+                    repository.deleteAgendaItem(it)
+                }
+            }
+        }
+    }
+
+    init {
+        savedStateHandle.get<AgendaItem>("agendaItem")?.let { item ->
+            setIsDone(item.isDone)
+            setTitle(item.title)
+            setDescription(item.description)
+            setStartTime(item.startDateAndTime.toLocalTime())
+            setStartDate(item.startDateAndTime.toLocalDate())
+            setSelectedReminderTime(item.reminderTime)
+        }
+        savedStateHandle.get<Boolean>("isInEditMode")?.let { initialEditMode ->
+            setEditMode(initialEditMode)
+        }
+    }
 }
