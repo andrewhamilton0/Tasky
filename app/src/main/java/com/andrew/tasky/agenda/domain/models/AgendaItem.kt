cb9717995378181d4b22e3f02bd90b0f1c03b0de
Andrew Hamilton <acjts425@gmail.com>
2023-01-14 06:22:55 -0500

    -Refactors Agenda Item into sealed interface

--- app/src/main/java/com/andrew/tasky/agenda/domain/models/AgendaItem.kt
+++ app/src/main/java/com/andrew/tasky/agenda/domain/models/AgendaItem.kt
@@ -2,27 +2,44 @@ package com.andrew.tasky.agenda.domain.models
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import com.andrew.tasky.agenda.util.AgendaItemType
 import com.andrew.tasky.agenda.util.ReminderTime
 import java.io.Serializable
 import java.time.LocalDateTime
 
-@Entity(
-    tableName = "agendaItems"
-)
+sealed interface AgendaItem : Serializable {
 
-data class AgendaItem(
-    @PrimaryKey(autoGenerate = true) var id: Int? = null,
-    val apiId: String,
-    val type: AgendaItemType,
-    var isDone: Boolean,
-    val title: String,
-    val description: String,
-    val startDateAndTime: LocalDateTime,
-    val endDateAndTime: LocalDateTime? = null,
-    val reminderTime: ReminderTime,
-    val photos: List<EventPhoto>? = emptyList(),
-    val isAttendee: Boolean? = null,
-    val attendees: List<Attendee>? = emptyList(),
-    val host: String? = null
-) : Serializable
+    @Entity
+    data class Event(
+        @PrimaryKey(autoGenerate = true) var id: String? = null,
+        var isDone: Boolean,
+        val title: String,
+        val description: String,
+        val startDateAndTime: LocalDateTime,
+        val endDateAndTime: LocalDateTime,
+        val reminderTime: ReminderTime,
+        val photos: List<EventPhoto>? = emptyList(),
+        val isAttendee: Boolean,
+        val attendees: List<Attendee>? = emptyList(),
+        val host: String
+    ) : AgendaItem
+
+    @Entity
+    data class Task(
+        @PrimaryKey(autoGenerate = true) var id: String? = null,
+        var isDone: Boolean,
+        val title: String,
+        val description: String,
+        val startDateAndTime: LocalDateTime,
+        val reminderTime: ReminderTime
+    ) : AgendaItem
+
+    @Entity
+    data class Reminder(
+        @PrimaryKey(autoGenerate = true) var id: String? = null,
+        var isDone: Boolean,
+        val title: String,
+        val description: String,
+        val startDateAndTime: LocalDateTime,
+        val reminderTime: ReminderTime
+    ) : AgendaItem
+}
