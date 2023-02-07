5b314844231082dace2c401220f58399568626ba
Andrew Hamilton <acjts425@gmail.com>
2023-02-07 15:35:19 -0500

    Sets up logout function

--- app/src/main/java/com/andrew/tasky/auth/data/AuthRepositoryImpl.kt
+++ app/src/main/java/com/andrew/tasky/auth/data/AuthRepositoryImpl.kt
@@ -1,11 +1,15 @@
 package com.andrew.tasky.auth.data
 
 import android.content.SharedPreferences
-import retrofit2.HttpException
+import com.andrew.tasky.agenda.domain.AgendaRepository
+import com.andrew.tasky.auth.domain.AuthRepository
+import com.andrew.tasky.auth.util.PrefsKeys
+import com.andrew.tasky.auth.util.getAuthResult
 
 class AuthRepositoryImpl(
     private val api: AuthApi,
-    private val prefs: SharedPreferences
+    private val prefs: SharedPreferences,
+    private val agendaRepository: AgendaRepository
 ) : AuthRepository {
 
     override suspend fun register(
@@ -13,7 +17,7 @@ class AuthRepositoryImpl(
         email: String,
         password: String
     ): AuthResult<Unit> {
-        return try {
+        val result = getAuthResult {
             api.register(
                 request = RegisterRequest(
                     fullName = fullName,
@@ -22,19 +26,16 @@ class AuthRepositoryImpl(
                 )
             )
             login(email, password)
-        } catch (e: HttpException) {
-            if (e.code() == 401) {
-                AuthResult.Unauthorized()
-            } else {
-                AuthResult.UnknownError()
-            }
-        } catch (e: Exception) {
-            AuthResult.UnknownError()
+        }
+        return when (result) {
+            is AuthResult.Authorized -> AuthResult.Authorized()
+            is AuthResult.Unauthorized -> AuthResult.Unauthorized()
+            is AuthResult.UnknownError -> AuthResult.UnknownError()
         }
     }
 
     override suspend fun login(email: String, password: String): AuthResult<Unit> {
-        return try {
+        val result = getAuthResult {
             val response = api.login(
                 request = LoginRequest(
                     email = email,
@@ -42,33 +43,47 @@ class AuthRepositoryImpl(
                 )
             )
             prefs.edit()
-                .putString("jwt", response.token)
+                .putString(PrefsKeys.JWT, response.token)
+                .putString(PrefsKeys.USER_ID, response.userId)
+                .putString(PrefsKeys.FULL_NAME, response.fullName)
                 .apply()
-            AuthResult.Authorized()
-        } catch (e: HttpException) {
-            if (e.code() == 401) {
-                AuthResult.Unauthorized()
-            } else {
-                AuthResult.UnknownError()
-            }
-        } catch (e: Exception) {
-            AuthResult.UnknownError()
+        }
+        return when (result) {
+            is AuthResult.Authorized -> AuthResult.Authorized()
+            is AuthResult.Unauthorized -> AuthResult.Unauthorized()
+            is AuthResult.UnknownError -> AuthResult.UnknownError()
         }
     }
 
     override suspend fun authenticate(): AuthResult<Unit> {
-        return try {
-            val token = prefs.getString("jwt", null) ?: return AuthResult.Unauthorized()
+        val result = getAuthResult {
+            val token = prefs.getString(
+                PrefsKeys.JWT, null
+            ) ?: return AuthResult.Unauthorized()
             api.authenticate("Bearer $token")
-            AuthResult.Authorized()
-        } catch (e: HttpException) {
-            if (e.code() == 401) {
-                AuthResult.Unauthorized()
-            } else {
-                AuthResult.UnknownError()
+        }
+        return when (result) {
+            is AuthResult.Authorized -> AuthResult.Authorized()
+            is AuthResult.Unauthorized -> AuthResult.Unauthorized()
+            is AuthResult.UnknownError -> AuthResult.UnknownError()
+        }
+    }
+
+    override suspend fun logout(): AuthResult<Unit> {
+        val result = getAuthResult {
+            val token = prefs.getString(
+                PrefsKeys.JWT, null
+            ) ?: return AuthResult.Unauthorized()
+            api.logout("Bearer $token")
+        }
+        return when (result) {
+            is AuthResult.Authorized -> {
+                prefs.edit().clear().apply()
+                agendaRepository.deleteAllAgendaTables()
+                AuthResult.Authorized()
             }
-        } catch (e: Exception) {
-            AuthResult.UnknownError()
+            is AuthResult.Unauthorized -> AuthResult.Unauthorized()
+            is AuthResult.UnknownError -> AuthResult.UnknownError()
         }
     }
 }
