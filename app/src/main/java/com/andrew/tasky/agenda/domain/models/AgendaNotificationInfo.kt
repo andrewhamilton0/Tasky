548ba1b73e1e8f222e0aed62197dce88a2a3ce8f
Andrew Hamilton <acjts425@gmail.com>
2023-05-17 20:18:27 -0400

    -Changed AgendaNotificationInfo from android parcelable to kotlinx parcelable

--- app/src/main/java/com/andrew/tasky/agenda/domain/models/AgendaNotificationInfo.kt
+++ app/src/main/java/com/andrew/tasky/agenda/domain/models/AgendaNotificationInfo.kt
@@ -1,7 +1,7 @@
 package com.andrew.tasky.agenda.domain.models
 
 import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
+import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class AgendaNotificationInfo(
